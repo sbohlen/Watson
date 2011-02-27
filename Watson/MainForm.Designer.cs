52d89255078f7152a89eeb557aadb075fb34d90f
Steve Bohlen <sbohlen@gmail.com>
2011-02-27 10:20:46 -0500

    initial commit -- v1 as used in CodeCamp Winter 2011

---
+++ Watson/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Watson
+{
+    partial class Watson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Watson));
+            this.btnSelectWinner = new System.Windows.Forms.Button();
+            this.txtTheWinner = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectWinner
+            // 
+            this.btnSelectWinner.BackColor = System.Drawing.Color.Green;
+            this.btnSelectWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectWinner.ForeColor = System.Drawing.Color.White;
+            this.btnSelectWinner.Location = new System.Drawing.Point(226, 402);
+            this.btnSelectWinner.Name = "btnSelectWinner";
+            this.btnSelectWinner.Size = new System.Drawing.Size(332, 80);
+            this.btnSelectWinner.TabIndex = 0;
+            this.btnSelectWinner.Text = "Pick a Winner";
+            this.btnSelectWinner.UseVisualStyleBackColor = false;
+            this.btnSelectWinner.Click += new System.EventHandler(this.btnSelectWinner_Click);
+            // 
+            // txtTheWinner
+            // 
+            this.txtTheWinner.BackColor = System.Drawing.Color.Black;
+            this.txtTheWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTheWinner.ForeColor = System.Drawing.Color.White;
+            this.txtTheWinner.Location = new System.Drawing.Point(12, 12);
+            this.txtTheWinner.Multiline = true;
+            this.txtTheWinner.Name = "txtTheWinner";
+            this.txtTheWinner.Size = new System.Drawing.Size(476, 350);
+            this.txtTheWinner.TabIndex = 2;
+            this.txtTheWinner.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(494, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 350);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Watson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 494);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtTheWinner);
+            this.Controls.Add(this.btnSelectWinner);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Watson";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Watson";
+            this.Load += new System.EventHandler(this.Watson_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectWinner;
+        private System.Windows.Forms.TextBox txtTheWinner;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Watson/MainForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace Watson
+{
+    public partial class Watson : Form
+    {
+        private IEnumerable<string> _items;
+        private readonly Random _generator = new Random();
+
+        public Watson()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadAndParseDataFile()
+        {
+            var data = ParseCsv("data.txt");
+            _items = ProcessData(data);
+
+        }
+
+        private IEnumerable<string> ProcessData(IEnumerable<string> data)
+        {
+            foreach (var item in data)
+            {
+                var corrected = item.Replace("\t", Environment.NewLine);
+                yield return corrected;
+            }
+            
+        }
+
+        private List<string> ParseCsv(string path)
+        {
+            var data = new List<string>();
+
+            try
+            {
+                using (var readFile = new StreamReader(path))
+                {
+                    string line;
+
+                    while ((line = readFile.ReadLine()) != null)
+                    {
+                        data.Add(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            return data;
+        }
+
+        private void SetInitialControlState()
+        {
+            txtTheWinner.Clear();
+        }
+
+        private void Watson_Load(object sender, EventArgs e)
+        {
+            SetInitialControlState();
+            LoadAndParseDataFile();
+        }
+
+        private void btnSelectWinner_Click(object sender, EventArgs e)
+        {
+            txtTheWinner.Text = _items.ElementAt(_generator.Next(1, _items.Count()));
+        }
+    }
+}
